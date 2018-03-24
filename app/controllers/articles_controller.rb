bfc2782a634c5eb6313f2c46bf1b83aeb7221011
Rails Udemy Course <rogervillanueva@outlook.com>
2018-03-24 21:40:13 +0000

    modified articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController 
+  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def new
     @article = Article.new
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def create
@@ -18,11 +20,9 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -36,13 +36,16 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
   
   def article_params
     params.require(:article).permit(:title, :description)
