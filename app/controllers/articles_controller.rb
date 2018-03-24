eab04fdb65023abcb72d89ac648767e50ae28814
Rails Udemy Course <rogervillanueva@outlook.com>
2018-03-24 20:53:28 +0000

    Added edit article, articles index, modified articles controller and article show/new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -17,8 +21,27 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  def index 
+    @articles = Article.all
+  end
+  
   private
+  
   def article_params
     params.require(:article).permit(:title, :description)
   end
+  
+  
+
+  
 end
